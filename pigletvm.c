98a9dc0443bd12e8340ac83d7c8d0c676da9b174
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2018-10-19 10:27:42 +0100

    Fix the range check in the usual interpreter

--- pigletvm.c
+++ pigletvm.c
@@ -58,165 +58,165 @@ interpret_result vm_interpret(uint8_t *bytecode)
 
     for (;;) {
         uint8_t instruction = NEXT_OP();
-            switch (instruction) {
-            case OP_PUSHI: {
-                /* get the argument, push it onto stack */
-                uint16_t arg = NEXT_ARG();
-                PUSH(arg);
-                break;
-            }
-            case OP_LOADI: {
-                /* get the argument, use it to get a value onto stack */
-                uint16_t addr = NEXT_ARG();
-                uint64_t val = vm.memory[addr];
-                PUSH(val);
-                break;
-            }
-            case OP_LOADADDI: {
-                /* get the argument, add the value from the address to the top of the stack */
-                uint16_t addr = NEXT_ARG();
-                uint64_t val = vm.memory[addr];
-                *TOS_PTR() += val;
-                break;
-            }
-            case OP_STOREI: {
-                /* get the argument, use it to get a value of the stack into a memory cell */
-                uint16_t addr = NEXT_ARG();
-                uint64_t val = POP();
-                vm.memory[addr] = val;
-                break;
-            }
-            case OP_LOAD: {
-                /* pop an address, use it to get a value onto stack */
-                uint16_t addr = POP();
-                uint64_t val = vm.memory[addr];
-                PUSH(val);
-                break;
-            }
-            case OP_STORE: {
-                /* pop a value, pop an adress, put a value into an address */
-                uint64_t val = POP();
-                uint16_t addr = POP();
-                vm.memory[addr] = val;
-                break;
-            }
-            case OP_DUP:{
-                /* duplicate the top of the stack */
-                PUSH(PEEK());
-                break;
-            }
-            case OP_DISCARD: {
-                /* discard the top of the stack */
-                (void)POP();
-                break;
-            }
-            case OP_ADD: {
-                /* Pop 2 values, add 'em, push the result back to the stack */
-                uint64_t arg_right = POP();
-                *TOS_PTR() += arg_right;
-                break;
-            }
-            case OP_ADDI: {
-                /* Add immediate value to the top of the stack */
-                uint16_t arg_right = NEXT_ARG();
-                *TOS_PTR() += arg_right;
-                break;
-            }
-            case OP_SUB: {
-                /* Pop 2 values, subtract 'em, push the result back to the stack */
-                uint64_t arg_right = POP();
-                *TOS_PTR() -= arg_right;
-                break;
-            }
-            case OP_DIV: {
-                /* Pop 2 values, divide 'em, push the result back to the stack */
-                uint64_t arg_right = POP();
-                /* Don't forget to handle the div by zero error */
-                if (arg_right == 0)
-                    return ERROR_DIVISION_BY_ZERO;
-                *TOS_PTR() /= arg_right;
-                break;
-            }
-            case OP_MUL: {
-                /* Pop 2 values, multiply 'em, push the result back to the stack */
-                uint64_t arg_right = POP();
-                *TOS_PTR() *= arg_right;
-                break;
-            }
-            case OP_JUMP:{
-                /* Use arg as a jump target  */
-                uint16_t target = PEEK_ARG();
-                vm.ip = bytecode + target;
-                break;
-            }
-            case OP_JUMP_IF_TRUE:{
-                /* Use arg as a jump target  */
-                uint16_t target = NEXT_ARG();
-                if (POP())
-                    vm.ip = bytecode + target;
-                break;
-            }
-            case OP_JUMP_IF_FALSE:{
-                /* Use arg as a jump target  */
-                uint16_t target = NEXT_ARG();
-                if (!POP())
-                    vm.ip = bytecode + target;
-                break;
-            }
-            case OP_EQUAL:{
-                uint64_t arg_right = POP();
-                *TOS_PTR() = PEEK() == arg_right;
-                break;
-            }
-            case OP_LESS:{
-                uint64_t arg_right = POP();
-                *TOS_PTR() = PEEK() < arg_right;
-                break;
-            }
-            case OP_LESS_OR_EQUAL:{
-                uint64_t arg_right = POP();
-                *TOS_PTR() = PEEK() <= arg_right;
-                break;
-            }
-            case OP_GREATER:{
-                uint64_t arg_right = POP();
-                *TOS_PTR() = PEEK() > arg_right;
-                break;
-            }
-            case OP_GREATER_OR_EQUAL:{
-                uint64_t arg_right = POP();
-                *TOS_PTR() = PEEK() >= arg_right;
-                break;
-            }
-            case OP_GREATER_OR_EQUALI:{
-                uint64_t arg_right = NEXT_ARG();
-                *TOS_PTR() = PEEK() >= arg_right;
-                break;
-            }
-            case OP_POP_RES: {
-                /* Pop the top of the stack, set it as a result value */
-                uint64_t res = POP();
-                vm.result = res;
-                break;
-            }
-            case OP_DONE: {
-                return SUCCESS;
-            }
-            case OP_PRINT:{
-                uint64_t arg = POP();
-                printf("%" PRIu64 "\n", arg);
-                break;
-            }
-            case OP_ABORT: {
-                return ERROR_END_OF_STREAM;
-            }
-            default:
-                return ERROR_UNKNOWN_OPCODE;
-            }
+        switch (instruction & 0x1f) {
+        case OP_PUSHI: {
+            /* get the argument, push it onto stack */
+            uint16_t arg = NEXT_ARG();
+            PUSH(arg);
+            break;
         }
-
-        return SUCCESS;
+        case OP_LOADI: {
+            /* get the argument, use it to get a value onto stack */
+            uint16_t addr = NEXT_ARG();
+            uint64_t val = vm.memory[addr];
+            PUSH(val);
+            break;
+        }
+        case OP_LOADADDI: {
+            /* get the argument, add the value from the address to the top of the stack */
+            uint16_t addr = NEXT_ARG();
+            uint64_t val = vm.memory[addr];
+            *TOS_PTR() += val;
+            break;
+        }
+        case OP_STOREI: {
+            /* get the argument, use it to get a value of the stack into a memory cell */
+            uint16_t addr = NEXT_ARG();
+            uint64_t val = POP();
+            vm.memory[addr] = val;
+            break;
+        }
+        case OP_LOAD: {
+            /* pop an address, use it to get a value onto stack */
+            uint16_t addr = POP();
+            uint64_t val = vm.memory[addr];
+            PUSH(val);
+            break;
+        }
+        case OP_STORE: {
+            /* pop a value, pop an adress, put a value into an address */
+            uint64_t val = POP();
+            uint16_t addr = POP();
+            vm.memory[addr] = val;
+            break;
+        }
+        case OP_DUP:{
+            /* duplicate the top of the stack */
+            PUSH(PEEK());
+            break;
+        }
+        case OP_DISCARD: {
+            /* discard the top of the stack */
+            (void)POP();
+            break;
+        }
+        case OP_ADD: {
+            /* Pop 2 values, add 'em, push the result back to the stack */
+            uint64_t arg_right = POP();
+            *TOS_PTR() += arg_right;
+            break;
+        }
+        case OP_ADDI: {
+            /* Add immediate value to the top of the stack */
+            uint16_t arg_right = NEXT_ARG();
+            *TOS_PTR() += arg_right;
+            break;
+        }
+        case OP_SUB: {
+            /* Pop 2 values, subtract 'em, push the result back to the stack */
+            uint64_t arg_right = POP();
+            *TOS_PTR() -= arg_right;
+            break;
+        }
+        case OP_DIV: {
+            /* Pop 2 values, divide 'em, push the result back to the stack */
+            uint64_t arg_right = POP();
+            /* Don't forget to handle the div by zero error */
+            if (arg_right == 0)
+                return ERROR_DIVISION_BY_ZERO;
+            *TOS_PTR() /= arg_right;
+            break;
+        }
+        case OP_MUL: {
+            /* Pop 2 values, multiply 'em, push the result back to the stack */
+            uint64_t arg_right = POP();
+            *TOS_PTR() *= arg_right;
+            break;
+        }
+        case OP_JUMP:{
+            /* Use arg as a jump target  */
+            uint16_t target = PEEK_ARG();
+            vm.ip = bytecode + target;
+            break;
+        }
+        case OP_JUMP_IF_TRUE:{
+            /* Use arg as a jump target  */
+            uint16_t target = NEXT_ARG();
+            if (POP())
+                vm.ip = bytecode + target;
+            break;
+        }
+        case OP_JUMP_IF_FALSE:{
+            /* Use arg as a jump target  */
+            uint16_t target = NEXT_ARG();
+            if (!POP())
+                vm.ip = bytecode + target;
+            break;
+        }
+        case OP_EQUAL:{
+            uint64_t arg_right = POP();
+            *TOS_PTR() = PEEK() == arg_right;
+            break;
+        }
+        case OP_LESS:{
+            uint64_t arg_right = POP();
+            *TOS_PTR() = PEEK() < arg_right;
+            break;
+        }
+        case OP_LESS_OR_EQUAL:{
+            uint64_t arg_right = POP();
+            *TOS_PTR() = PEEK() <= arg_right;
+            break;
+        }
+        case OP_GREATER:{
+            uint64_t arg_right = POP();
+            *TOS_PTR() = PEEK() > arg_right;
+            break;
+        }
+        case OP_GREATER_OR_EQUAL:{
+            uint64_t arg_right = POP();
+            *TOS_PTR() = PEEK() >= arg_right;
+            break;
+        }
+        case OP_GREATER_OR_EQUALI:{
+            uint64_t arg_right = NEXT_ARG();
+            *TOS_PTR() = PEEK() >= arg_right;
+            break;
+        }
+        case OP_POP_RES: {
+            /* Pop the top of the stack, set it as a result value */
+            uint64_t res = POP();
+            vm.result = res;
+            break;
+        }
+        case OP_DONE: {
+            return SUCCESS;
+        }
+        case OP_PRINT:{
+            uint64_t arg = POP();
+            printf("%" PRIu64 "\n", arg);
+            break;
+        }
+        case OP_ABORT: {
+            return ERROR_END_OF_STREAM;
+        }
+        case 26 ... 0x1f:
+            return ERROR_UNKNOWN_OPCODE;
+        }
     }
+
+    return ERROR_END_OF_STREAM;
+}
 
 interpret_result vm_interpret_threaded(uint8_t *bytecode)
 {
