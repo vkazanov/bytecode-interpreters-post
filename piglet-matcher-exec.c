2dda71e9c18dc8a992092f3138c8d709c4d8b053
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2018-11-27 08:05:44 +0000

    more scaffolding

--- piglet-matcher-exec.c
+++ piglet-matcher-exec.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "piglet-matcher.h"
 
 int main(int argc, char *argv[])
 {
-    (void) argc; (void) argv;
-    return 0;
+    if (argc < 3) {
+        fprintf(stderr, "Usage: <asm|dis|run> [arg1 [arg2 ...]]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    const char *cmd = argv[1];
+
+    int res = EXIT_SUCCESS;
+    if (0 == strcmp(cmd, "asm")) {
+        if (argc != 4) {
+            fprintf(stderr, "Usage: asm <path/to/asm> <path/to/output/bytecode>\n");
+            exit(EXIT_FAILURE);
+        }
+        /* TODO: asm */
+    } else if (0 == strcmp(cmd, "run")) {
+        if (argc != 4) {
+            fprintf(stderr, "Usage: run <path/to/bytecode> <path/to/input>\n");
+            exit(EXIT_FAILURE);
+        }
+        /* TODO: run */
+    } else if (0 == strcmp(cmd, "dis")) {
+        if (argc != 3) {
+            fprintf(stderr, "Usage: dis <path/to/bytecode>\n");
+            exit(EXIT_FAILURE);
+        }
+        /* TODO: disasm */
+    } else {
+        fprintf(stderr, "Unknown cmd: %s\n", cmd);;
+        res = EXIT_FAILURE;
+    }
+
+    return res;
 }
